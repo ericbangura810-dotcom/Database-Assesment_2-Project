2d4be1447bdcf4739eea60c6090d6d00ea597364
Sho <ericbangura810@gmail.com>
2026-04-25 17:15:58 +0100

    update vehicle status to in_use when assigned

--- model_fleet.py
+++ model_fleet.py
@@ -4,3 +4,16 @@ from db import get_connection
 def assign_driver_to_shipment(shipment_id, driver_id, vehicle_id, route_details):
     conn = get_connection()
     cur = conn.cursor()
+
+    assigned_at = datetime.now().isoformat()
+
+    cur.execute("""
+    INSERT INTO delivery_records (
+    shipment_id, driver_id, vehicle_id, route_details, assigned_at, current_status )
+    VALUES (?, ?, ?, ?, ?, 'in_transit') """, (shipment_id, driver_id, vehicle_id, route_details, assigned_at))
+
+    cur.execute("""
+        UPDATE vehicles
+        SET status = 'in_use'
+        WHERE id = ? 
+    """, (vehicle_id,))
