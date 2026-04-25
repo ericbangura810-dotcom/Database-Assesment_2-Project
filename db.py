ee5709a846c008a97cff365f0faa0581a9a7a170
Sho <ericbangura810@gmail.com>
2026-04-25 12:41:43 +0100

    Initialised Database

--- db.py
+++ db.py
@@ -2,3 +2,17 @@ import sqlite3
 import os
 
 DB_NAME = os.path.join(os.path.dirname(__file__), 'northshore_locations.db')
+
+print("Using DB at:", DB_NAME)
+
+def get_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_db():
+    conn = get_connection()
+    cur = conn.cursor()
+
+
