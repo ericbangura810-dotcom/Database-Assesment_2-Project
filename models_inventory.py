f1c3eee8cce786c34949c6683d6c5d1209b436c6
Sho <ericbangura810@gmail.com>
2026-05-02 17:51:56 +0100

    log warehouse activity for inventory changes

--- models_inventory.py
+++ models_inventory.py
@@ -15,7 +15,15 @@ def adjust_inventory(warehouse_id, item_id, quantity_change, activity_type, rela
     UPDATE warehous_inventory 
     SET quantity = quantity + ?
     WHERE warehouse_id = ? AND item_id = ?
-    """, (quantity_change, warehouse_id, item_id)
+    """, (quantity_change, warehouse_id, item_id))
+
+    cur.execute("""
+    INSERT INTO warehoue_activity_logs (warehouse_id, activity_type, item_id, quantity_change, related_shipment_id, timestamp, performed_by_user_id)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+    """,(warehouse_id, activity_type, item_id, quantity_change, related_shipment_id, datetime.now(), performed_by_user_id))
+
+    conn.commit()
+    conn.close()
 
 
 
