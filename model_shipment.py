95e93b884ddf8aab1fe85ea1cba14759219ec5c7
Sho <ericbangura810@gmail.com>
2026-05-02 17:26:44 +0100

    added shipment insert 'in transit' status

--- model_shipment.py
+++ model_shipment.py
@@ -3,8 +3,26 @@ from db import get_connection
 
 def add_shipment(order_number, sender_name, sender_address_encrypted,
                  receiver_name, receiver_address_encrypted, item_description,
-                 origin_warehouse, destination_warehouse_id,
+                 origin_warehouse_id, destination_warehouse_id,
                  expected_delivery_date):
 
     conn = get_connection()
     cur = conn.cursor()
+
+    cur.execute("""
+      INSERT INTO shipments (
+      order_number, sender_name, sender_address_encrypted,
+      receiver_name, receiver_address_encrypted, item_description,
+      origin_warehouse_id, destination_warehouse_id,
+      status, created_at, expected_delivery_date
+      ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'in transit' , ?, ?)
+      """, (
+        order_number, sender_name, sender_address_encrypted,
+        receiver_name, receiver_address_encrypted, item_description,
+        origin_warehouse_id, destination_warehouse_id,
+        datetime.now().isoformat(), expected_delivery_date
+    ))
+
+
+
+
