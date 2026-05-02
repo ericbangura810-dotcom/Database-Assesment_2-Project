a18a715149bb31ed3cbc6ea8d2fd05802dfc3019
Sho <ericbangura810@gmail.com>
2026-05-02 18:41:32 +0100

    added text encryption and decryption

--- security.py
+++ security.py
@@ -6,3 +6,15 @@ def hash_password(password):
 
 def check_password(password, stored_hash):
     return hash_password(password) == stored_hash
+
+KEY = 5
+
+def encrypt_text(text):
+    if text is None:
+        return None
+    return "".join(chr(ord(c) ^ KEY) for c in text)
+
+def decrypt_text(text):
+    if text is None:
+        return None
+    return "".join(chr(ord(c) ^ KEY) for c in text)
